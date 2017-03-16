4ca08a560ac7c39c748b911b77a21c7ce7ac1188
Ethan Heckard <eheckard23@gmail.com>
2017-03-16 14:40:32 -0400

    pinterst not finding page

--- assets/js/view.js
+++ assets/js/view.js
@@ -77,7 +77,7 @@ class View{
 			// create pinterest save button
 			$('.tools').append(
 					'<div class="pinterest">'
-					+ `<a data-pin-do="buttonBookmark" href="https://www.pinterest.com/pin/create/button/&media=${recipe.src}/&url=${window.location.href}">`
+					+ `<a data-pin-do="buttonPin" href="https://www.pinterest.com/pin/create/button/&media=${recipe.src}/&url=${window.location.href}">`
 					+ '<img src="//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_gray_20.png" />'
 					+ '</a>'
 					+ '</div>'
