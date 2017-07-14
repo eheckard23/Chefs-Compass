0e0538677a0d2c54771288fa2e27915c93ec2ea2
Ethan Heckard <eheckard23@gmail.com>
2017-07-13 21:10:09 -0400

    initial commit for unittest feature branch

--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
-## Deploying Process 
+## Deploying Using Branching Model
+* Begin by creating a fork of this repo for yourself.
+* 
 
---
+++ test/__basic.js
@@ -0,0 +1,13 @@
+const server = require('../server');
+const expect = require('chai').expect;
+
+describe('Server Test', () => {
+	it('should be running on the correct port');
+});
+
+describe('/index', () => {
+	it('should respond with index.html', (done) => {
+		// get / and find response
+		done();
+	});
+});
